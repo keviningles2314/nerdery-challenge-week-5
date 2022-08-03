ce585c00fb2d22be40e41d700b78a2ed2bed884e
Kevin Ingles <kfabian@applaudostudios.dev>
2022-08-03 14:29:51 -0600

    feat: add email validation to textbox

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,12 +1,38 @@
+import React, { useEffect, useState } from 'react';
 import './loginStyle.css';
+
 const Login = () => {
+  const [isbuttonDisabled, setIsButtonDisabled] = useState(false);
+  const [textUserEmail, setTextUserEmail] = useState('');
+
+  useEffect(() => {
+    if (/\S+@\S+\.\S+/.test(textUserEmail)) {
+      setIsButtonDisabled(false);
+    } else {
+      setIsButtonDisabled(true);
+    }
+  }, [textUserEmail]);
+
+  const handleTextChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTextUserEmail(event.target.value);
+  };
+
+  console.log(textUserEmail);
   return (
     <div className='Login'>
       <div className='login-form'>
         <div className='form-content'>
           <h1>Welcome</h1>
-          <input id='textBoxEmail' type='text' placeholder='user@ravn.com' />
+          <input
+            value={textUserEmail}
+            onChange={(e) => handleTextChange(e)}
+            id='textBoxEmail'
+            type='text'
+            placeholder='user@ravn.co'
+          />
           <button id='loginButton'>LOGIN</button>
+
+          <h3>{isbuttonDisabled ? 'disabled' : 'correct'}</h3>
         </div>
       </div>
     </div>
