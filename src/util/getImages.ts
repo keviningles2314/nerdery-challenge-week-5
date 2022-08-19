6994406a901fedfb5f83bb1aa837391fa38abaec
keviningles2314 <kevin.ingles2314@gmail.com>
2022-08-19 09:12:35 -0600

    delete src folder

--- src/util/getImages.ts
+++
@@ -1,7 +0,0 @@
-import { getImageById } from '../fetch/fetchImage';
-
-export const getPostImage = async (imageId: number | string | undefined) => {
-  const imageInformation = await getImageById(imageId);
-  return imageInformation;
-};
-//
--- src/util/getUser.ts
+++
@@ -1,7 +0,0 @@
-import { getUserById } from '../fetch/fetchUser';
-
-export const getUserInfo = async (userId: number) => {
-  const userInfo = await getUserById(userId);
-  return userInfo;
-};
-//
